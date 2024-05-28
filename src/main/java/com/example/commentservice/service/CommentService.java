00cc493d22055c551c8aeab638c47f7349016951
k0j09cl <Kinjal.Jain@walmart.com>
2024-05-28 13:20:30 +0530

    Added transaction

--- src/main/java/com/example/commentservice/service/CommentService.java
+++ src/main/java/com/example/commentservice/service/CommentService.java
@@ -17,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -24,6 +25,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional
 public class CommentService {
 
     @Autowired
